03df9196365c1c6422f91859d6abc314f691011f
edubasabe <edubasabe1@gmail.com>
2017-10-28 18:57:25 -0300

    Editando el plugin de FAQ trabajando en Single Product

--- wp-content/themes/tamed2017/woocommerce/myaccount/form-edit-account.php
+++ wp-content/themes/tamed2017/woocommerce/myaccount/form-edit-account.php
@@ -27,34 +27,34 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 	<?php do_action( 'woocommerce_edit_account_form_start' ); ?>
 
 	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-		<label for="account_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" value="<?php echo esc_attr( $user->first_name ); ?>" />
+		<!-- <label for="account_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label> -->
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" placeholder="<?php _e( 'First name', 'woocommerce' ); ?>" value="<?php echo esc_attr( $user->first_name ); ?>" />
 	</p>
 	<p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
-		<label for="account_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" value="<?php echo esc_attr( $user->last_name ); ?>" />
+		<!-- <label for="account_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label> -->
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" placeholder="<?php _e( 'Last name', 'woocommerce' ); ?>" value="<?php echo esc_attr( $user->last_name ); ?>" />
 	</p>
 	<div class="clear"></div>
 
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-		<label for="account_email"><?php _e( 'Email address', 'woocommerce' ); ?> <span class="required">*</span></label>
-		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" value="<?php echo esc_attr( $user->user_email ); ?>" />
+		<!-- <label for="account_email"><?php _e( 'Email address', 'woocommerce' ); ?> <span class="required">*</span></label> -->
+		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" placeholder="<?php _e( 'Email address', 'woocommerce' ); ?>" value="<?php echo esc_attr( $user->user_email ); ?>" />
 	</p>
 
 	<fieldset>
 		<legend><?php _e( 'Password change', 'woocommerce' ); ?></legend>
 
 		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-			<label for="password_current"><?php _e( 'Current password (leave blank to leave unchanged)', 'woocommerce' ); ?></label>
-			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_current" id="password_current" />
+			<!-- <label for="password_current"><?php _e( 'Current password (leave blank to leave unchanged)', 'woocommerce' ); ?></label> -->
+			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_current" placeholder="<?php _e( 'Current password (leave blank to leave unchanged)', 'woocommerce' ); ?>" id="password_current" />
 		</p>
 		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-			<label for="password_1"><?php _e( 'New password (leave blank to leave unchanged)', 'woocommerce' ); ?></label>
-			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_1" id="password_1" />
+			<!-- <label for="password_1"><?php _e( 'New password (leave blank to leave unchanged)', 'woocommerce' ); ?></label> -->
+			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_1" placeholder="<?php _e( 'New password (leave blank to leave unchanged)', 'woocommerce' ); ?>" id="password_1" />
 		</p>
 		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-			<label for="password_2"><?php _e( 'Confirm new password', 'woocommerce' ); ?></label>
-			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_2" id="password_2" />
+			<!-- <label for="password_2"><?php _e( 'Confirm new password', 'woocommerce' ); ?></label> -->
+			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_2" placeholder="<?php _e( 'Confirm new password', 'woocommerce' ); ?>" id="password_2" />
 		</p>
 	</fieldset>
 	<div class="clear"></div>
